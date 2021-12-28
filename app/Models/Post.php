0c367805c9c497aa9556bd500f2967d53f3a79c8
P-000242\WahWahWinShweHlaing <scm.wahwahwynnshwehlaing@gmail.com>
2021-12-28 22:10:43 +0630

    #section_07 Fix a Confusing Eloquent Query Bug commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -50,8 +50,9 @@ class Post extends Model
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? false, fn($query,$search) =>
+                $query->where(fn($query) => 
                     $query->where('title', 'like' , '%' . $search. '%')
-                          ->orwhere('body', 'like' , '%' . $search. '%'));
+                          ->orwhere('body', 'like' , '%' . $search. '%')));
 
         $query->when($filters['category'] ?? false, fn($query,$category) =>
                 $query->whereHas('category' , fn($query) =>
