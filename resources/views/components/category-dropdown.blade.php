6539891cff754f902245063114f20d2fd7faa984
P-000242\WahWahWinShweHlaing <scm.wahwahwynnshwehlaing@gmail.com>
2021-12-28 23:04:51 +0630

    #section_08 Laughably Simple Pagination commit

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,11 +7,12 @@
         </x-slot>
 
         <div x-show="show" class="py-2 absolute z-50" style="display : none">
-            <x-dropdown-item href="/" :active="request()->routeIs('home')">
+            <x-dropdown-item href="/?{{ http_build_query(request()->except('category','page')) }}" 
+                :active="request()->routeIs('home')">
                 ALL
             </x-dropdown-item>
                     @foreach($categories as $category)
-                        <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+                        <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category','page')) }}"
                             :active='request()->is("/categories/{$category->slug}")'>
                             {{ ucwords($category->name)}}
                         </x-dropdown-item>
