912fee312c556d024eb97fcf97170e7ba2f4f376
P-000242\WahWahWinShweHlaing <scm.wahwahwynnshwehlaing@gmail.com>
2021-12-29 19:34:59 +0630

    #section10 Design the Comment Form commit

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,22 +1,24 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100">
-    <div>
-        <img src="https://i.pravatar.cc/100?id={{ $comment->id }}" alt="" width="60" height="60">
-    </div>
-    <div>
-        <header class="mb-4">
-            <h3 class="fond-bold">
-               {{ $comment->author->username }}
-            </h3>
-            <p class="text-xs">
-                Posted<time>{{ $comment->created_at}}</time>
+<x-panel>
+    <article class="flex bg-gray-100">
+        <div>
+            <img src="https://i.pravatar.cc/100?id={{ $comment->id }}" alt="" width="60" height="60">
+        </div>
+        <div>
+            <header class="mb-4">
+                <h3 class="fond-bold">
+                {{ $comment->author->username }}
+                </h3>
+                <p class="text-xs">
+                    Posted<time>{{ $comment->created_at}}</time>
+                </p>
+            </header>
+
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
+        </div>
 
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-
-</article>
+    </article>
+</x-panel>
