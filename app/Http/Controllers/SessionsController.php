b4f53f2608c089212b827c2c3a79bf443f066c6e
P-000242\WahWahWinShweHlaing <scm.wahwahwynnshwehlaing@gmail.com>
2021-12-29 14:14:06 +0630

    #section_09 Build the Log In Page commit

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -1,10 +1,43 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    /**
+     * create function
+     * 
+     */
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    /**
+     * store function
+     * 
+     */
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email'=>'required|email',
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($attributes)){
+            session()->regenerate();
+            return redirect('/')->with('success','Welcome Back');
+        }
+
+        throw ValidationException::withMessages(['email' => 'Your email is invaild!!!!']);
+        // return back()->withErrors(['email' => 'Your email is invaild!!!!']);
+    }
+
+    /**
+     * destory function
+     * 
+     */
     public function destory()
     {
         auth()->logout();
--- routes/web.php
+++ routes/web.php
@@ -103,5 +103,8 @@ Route::get('authors/{author:username}', function (User $author) {
 //section_09 Build a Register User Page
 Route::get('register', [RegisterController::class,'create' ])->middleware('guest');
 Route::post('register', [RegisterController::class,'store' ])->middleware('guest');
-Route::post('logout', [SessionsController::class,'destory' ]);
+
+Route::get('login', [SessionsController::class,'create' ])->middleware('guest');
+Route::post('login', [SessionsController::class,'store' ])->middleware('guest');
+Route::post('logout', [SessionsController::class,'destory' ])->middleware('auth');
 
