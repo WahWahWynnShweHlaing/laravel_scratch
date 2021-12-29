53c8007db50d1e251c0a2146f1c2c3b33c053e72
P-000242\WahWahWinShweHlaing <scm.wahwahwynnshwehlaing@gmail.com>
2021-12-29 10:38:15 +0630

    #section_09 Failed Validation and Old Input Data commit

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -25,7 +25,7 @@ class RegisterController extends Controller
         // var_dump(request()->all());
         $attributes = request()->validate([
                         'name' => 'required|max:255',
-                        'username'=>'required|max:255|min:3',
+                        'username'=>'required|min:3|max:255|unique:users,username',
                         'email'=> 'required|email|max:255',
                         'password' => 'required|max:255|min:7'
                     ]);
