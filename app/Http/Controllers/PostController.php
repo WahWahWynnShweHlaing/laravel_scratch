2e90539d0319aa605de03211aec1a7736a102443
P-000242\WahWahWinShweHlaing <scm.wahwahwynnshwehlaing@gmail.com>
2021-12-30 13:47:44 +0630

    #section12 Limit Access to Only Admins update commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
 
 class PostController extends Controller
 {
@@ -30,12 +30,8 @@ class PostController extends Controller
         ]);
     }
 
-    protected function create()
+    public function create()
     {
-
-        If(auth()->user()?->username != 'JISUNG1'){
-            abort(Response::HTTP_FORBIDDEN);
-        }
         return view('posts.create');
     }
 }
